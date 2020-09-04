b4a97ff78438976ccb8ed844f683d0376b0ec002
Chentian Jiang <chentian.jiang@gmail.com>
2020-09-04 02:58:00 +0200

    make reproducible and update to be compatible with the merging script

--- activity_prediction/splitting.py
+++ activity_prediction/splitting.py
@@ -6,7 +6,7 @@ import json
 random.seed(0)
 
 from preprocessing import helperfuns
-from preprocessing import joining
+from preprocessing import merging
 
 CLEANED_STEPS_PATH = "preprocessing/clean_data/steps_minutes_df.pickle"
 CLEANED_HR_PATH = "preprocessing/clean_data/hr_seconds_df.pickle"
@@ -39,32 +39,35 @@ def main():
 
     # for each row in steps_df, match labels that occur within 1hr
     # before the row's timestamp
-    merged = joining.merge_labels(
-        steps_df,
-        tolerance=pd.Timedelta("1H"),
-        labels_path="preprocessing/clean_data/labels_df.pickle",
+    merged = merging.merge_labels(
+        steps_df, labels_path="preprocessing/clean_data/labels_df.pickle",
     )
 
-    all_participants = set(merged.Id.unique())
+    all_participants = set(merged.index.get_level_values(0).unique())
 
     # find participants without any arm (strength/aerobic/combined)
     # labels, i.e. they only have the "other" label
-    unique_labels = merged.groupby("Id")["Arm"].apply(lambda x: set(x.unique()))
+    unique_labels = merged.groupby(level="Id")["Arm"].apply(lambda x: set(x.unique()))
     no_arms_participants = set(unique_labels[unique_labels == {"other"}].index)
 
     # find participants in the "combined" arm
-    combined_participants = set(merged[merged["Arm"] == "combined"]["Id"].unique())
+    combined_participants = set(
+        merged[merged["Arm"] == "combined"].index.get_level_values(0).unique()
+    )
 
     # only keep participants that have labels and that are *not* in the
     # "combined" arm
-    keep_participants = all_participants - no_arms_participants - combined_participants
+    keep_participants = list(
+        all_participants - no_arms_participants - combined_participants
+    )
+    keep_participants.sort()  # so that the random seed will reproduce the same splits
     num_participants = len(keep_participants)
 
     # separate train vs test participants
     test_num = int(num_participants * TEST_PARTICIPANT_RATIO)
     split_dict = dict()
     split_dict["test"] = random.sample(keep_participants, test_num)
-    split_dict["train"] = list(keep_participants - set(split_dict["test"]))
+    split_dict["train"] = list(set(keep_participants) - set(split_dict["test"]))
     split_dict["test"].sort()
     split_dict["train"].sort()
 
